454146c4fab30caca7b973b4a429e8c32c9b7afa
Oleksandr Karpiuk <alex.karpuyk@gmail.com>
2023-06-29 14:37:13 +0300

    Finished project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # goit-node-hw-01
- 
+
+Даний файл оформлено для здачі домашнього завдання №1
+Скріншоти команд виконано згідно з кроком 5 ТЗ до домашнього завдання, а саме:
+
+1. [node index.js --action="list"](https://monosnap.com/file/NK4BF6djzPcMSkscKOn0iVip4Frr0v)
+2. [node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6](https://monosnap.com/file/ZD96VtqjT9bxkhMnN8P1QykO1sQ13P)
+3. [node index.js --action="add" --name Mango --email mango@gmail.com --phone 322-22-22](https://monosnap.com/file/duIAI7jEcGTYqBdtxAZeUyVjpxx61W)
+4. [node index.js --action="remove" --id qdggE76Jtbfd9eWJHrssH](https://monosnap.com/file/CVlRf8Fp9glcYvDh7OqbXaAfeF2WO6)
--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("Hi, it's work! Test #3");
+const contacts = require("./contacts");
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.log(allContacts);
+      break;
+
+    case "get":
+      const contact = await contacts.getContactById(id);
+      console.log(contact);
+      break;
+
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removedContact = await contacts.removeContact(id);
+      console.log(removedContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+const arr = hideBin(process.argv);
+const { argv } = yargs(arr);
+invokeAction(argv);
